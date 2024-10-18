59d4103db1827239b5efec9587f417baf1a112e8
kschulz20 <zerochubs96@gmail.com>
2024-10-17 18:36:43 -0700

    rotating hitbox around player

--- src/Entities/Player/cane.gd
+++ src/Entities/Player/cane.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var CANE_HITBOX_DISTANCE_FROM_PLAYER: float = 170
 @onready var _animation_player = $AnimationPlayer
 @onready var cane_sprite = $CaneSprite
 
@@ -13,6 +14,8 @@ func _process(delta: float) -> void:
 	if (not _animation_player.is_playing()):
 		cane_sprite.visible = false
 
-func _on_player_character_cane_attack() -> void:
+func _on_player_character_cane_attack(cane_hitbox_vector: Vector2) -> void:
+	cane_sprite.position = cane_hitbox_vector * CANE_HITBOX_DISTANCE_FROM_PLAYER
+	cane_sprite.rotation = cane_hitbox_vector.angle()
 	cane_sprite.visible = true
 	_animation_player.play("cane_attack")
--- src/Entities/Player/player_character.gd
+++ src/Entities/Player/player_character.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var move_speed :float = 1600.0
 
 var is_mouse_input = false
-signal cane_attack
+signal cane_attack(cane_hitbox_vector: Vector2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,18 +15,32 @@ func _input(event: InputEvent) -> void:
 		is_mouse_input = true
 	if event is InputEventJoypadMotion:
 		is_mouse_input = false
+		#
+			## aim input
+	#var aim_dir :Vector2 = Vector2(0.0, 0.0)
+	#if is_mouse_input:
+		#var mouse_position :Vector2 = self.get_local_mouse_position()
+		#aim_dir = mouse_position.normalized()
+	#else:
+		#aim_dir	= Input.get_vector(
+			#"aim_left", "aim_right",
+			#"aim_up", "aim_down"
+		#).normalized()
+	#
+	#if is_mouse_input:
+		## print("mouse aim: " + str(aim_dir))
+		#pass
+	#else:
+		## print("controller aim: " + str(aim_dir))
+		#pass
+#
+	#if (event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT):
+		#var mouse_angle: float = aim_dir.angle()
+		#var cane_hitbox_vector: Vector2 = Vector2.from_angle(mouse_angle)
+		## multiply by some amount here
+		#cane_attack.emit(cane_hitbox_vector)
 
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("cane_attack")):
-		cane_attack.emit()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	# movement input
-	var vel_dir :Vector2 = Input.get_vector(
-		"move_left", "move_right",
-		"move_up", "move_down"
-	).normalized()
 	# aim input
 	var aim_dir :Vector2 = Vector2(0.0, 0.0)
 	if is_mouse_input:
@@ -44,7 +58,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		# print("controller aim: " + str(aim_dir))
 		pass
-		
+
+	if (Input.is_action_just_pressed("cane_attack")):
+		var mouse_angle: float = aim_dir.angle()
+		var cane_hitbox_vector: Vector2 = Vector2.from_angle(mouse_angle)
+		# multiply by some amount here
+		cane_attack.emit(cane_hitbox_vector)
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	# movement input
+	var vel_dir :Vector2 = Input.get_vector(
+		"move_left", "move_right",
+		"move_up", "move_down"
+	).normalized()
 	
 	# handle movement
 	velocity = move_speed * delta * vel_dir;
