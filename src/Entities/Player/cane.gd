db27564757dfc5b05bbd8b965ccdeb22a7d40c79
kschulz20 <zerochubs96@gmail.com>
2024-10-17 18:49:58 -0700

    flipping cane animation depending on where player clicked

--- src/Entities/Player/cane.gd
+++ src/Entities/Player/cane.gd
@@ -8,14 +8,18 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (not _animation_player.is_playing()):
 		cane_sprite.visible = false
 
 func _on_player_character_cane_attack(cane_hitbox_vector: Vector2) -> void:
+	# Distance cane attack sprite away from player
 	cane_sprite.position = cane_hitbox_vector * CANE_HITBOX_DISTANCE_FROM_PLAYER
+	# Change animation direciton of cane attack based on where mouse is
+	cane_sprite.set_flip_v(cane_hitbox_vector.x < 0)
+	# Rotate cane sprite attack based off where player clicked
 	cane_sprite.rotation = cane_hitbox_vector.angle()
+	
 	cane_sprite.visible = true
 	_animation_player.play("cane_attack")
