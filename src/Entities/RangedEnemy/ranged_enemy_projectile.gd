cf00f7777a150e8383429c68498fdc6ff883df55
kschulz20 <zerochubs96@gmail.com>
2024-10-22 17:46:13 -0700

    projectile explodes on contact with player

--- src/Entities/RangedEnemy/ranged_enemy_projectile.gd
+++ src/Entities/RangedEnemy/ranged_enemy_projectile.gd
@@ -8,6 +8,7 @@ var team = ""
 func _ready():
 	add_to_group("enemy")
 	$ProjectileLifetime.start()
+	$AnimatedSprite2D.play("fired")
 
 func _physics_process(delta: float) -> void:
 	position += heading.normalized() * delta * speed
@@ -16,7 +17,12 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.get("health"):
 		if (body.is_in_group("player")):
 			SignalBus.player_take_damage.emit(damage)
-			call_deferred("queue_free")
+			$AnimatedSprite2D.play("explosion")
+			speed = 0
 
 func _on_projectile_lifetime_timeout() -> void:
+	call_deferred("queue_free")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
 	call_deferred("queue_free")
